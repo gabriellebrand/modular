8e1aeda8013ebe58b317c90a46437155ee564026
Wellington Bezerra <bezwellington@gmail.com>
2017-10-07 00:52:33 -0300

    Módulo Perfil: Colocando o módulo no formato exigido

--- arcaboucoteste_2_02/perfil/PERFIL.c
+++ arcaboucoteste_2_02/perfil/PERFIL.c
@@ -1,38 +1,71 @@
 /***************************************************************************
-*  $MCI Módulo de implementação: PERFIL  Módulo Perfil
-*
-*  Arquivo gerado:              PERFIL.c
-*  Letras identificadoras:      PERFIL
-*
-*  Nome da base de software:    ArcabouÁo para a automaÁ„o de testes de programas redigidos em C
-*  Arquivo da base de software: D:\AUTOTEST\PROJETOS\LISTA.BSW
-*
-*  Projeto: INF 1301 / 1628 AutomatizaÁ„o dos testes de mÛdulos C
-*  Gestor:  LES/DI/PUC-Rio
-*  Autores: wbs
-*
-*  $HA Histórico de evolução:
-*     Versão  Autor    Data     Observações
-*     1       wbs   02/out/2017 Início do desenvolvimento
-*
-***************************************************************************/
+ *  $MCI Módulo de implementaçăo: PER  Perfil
+ *
+ *  Arquivo gerado:              PERFIL.c
+ *  Letras identificadoras:      PER
+ *
+ *  Nome da base de software:    Arcabouço para a automaçăo de testes de programas redigidos em C
+ *
+ *  Projeto: Trabalho 2 - Programaçăo Modular
+ *  Autores: GB - Gabrielle Brandenburg
+    GC - Gabriel Cantergiani
+    WB - Wellingotn Bezerra
+ *
+ *  $HA Histórico de evoluçăo:
+ *     Versăo      Autor            Data                Observaçőes
+ *     1.0       GB,GC,WB        02/out/2017       Criação do módulo
+ *
+ ***************************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <malloc.h>
 #include <string.h>
 
+#define PERFIL_OWN
 #include "PERFIL.h"
+#undef PERFIL_OWN
 
-struct perfil {
+/***********************************************************************
+ *
+ *  $TC Tipo de dados: PER Perfil
+ *
+ *
+ ***********************************************************************/
 
-	char nome[100];
-	char email[20];
-	char cidade[20];
-	int idade;
+typedef struct PER_tagPerfil {
 
-};
+    char nome[100];
+        /* Nome do perfil
+         */
+    
+    char email[20];
+        /* Email do perfil
+         */
+    
+    char cidade[20];
+        /* Cidade do perfil
+         */
+    
+    int idade;
+        /* Idade do perfil
+         */
+    
+    int id;
+        /* Id do perfil
+         */
 
-Perfil* criaPerfil(char *nome, char *email, char *cidade, int idade) {
+} PER_tpPerfil ;
+
+/*****  Código das funções exportadas pelo módulo  *****/
+
+/***************************************************************************
+ *
+ *  Função: PER Criar Perfil
+ *****/
+
+Perfil* PER_CriarPerfil(char *nome, char *email, char *cidade, int idade)
+{
     
 	Perfil *p;
 
@@ -40,21 +73,36 @@ Perfil* criaPerfil(char *nome, char *email, char *cidade, int idade) {
 	if( p == NULL ){
 		printf("Espaco insuficiente");
 		exit(1);
-	}
+	} /* if */
+    
 	strcpy(p->nome, nome);
 	strcpy(p->email, email);
 	strcpy(p->cidade, cidade);
 	p->idade = idade;
 
 	return p;
-}
+    
+} /* Fim função: PER  Criar Perfil */
 
-void mostraPerfil(Perfil *p) {
+
+/***************************************************************************
+ *
+ *  Função: PER Mostrar Perfil
+ *****/
+
+void PER_MostrarPerfil(Perfil *p) {
 	printf("nome: %s, email: %s, cidade: %s, idade: %d", p->nome, p->email, p->cidade, p->idade);
 
-}
+} /* Fim função: PER  Mostrar Perfil */
 
-void destroiPerfil(Perfil *p) {
+/***************************************************************************
+ *
+ *  Função: PER Destruir Perfil
+ *****/
+
+void PER_destroirPerfil(Perfil *p) {
 	free(p);
 
-}
+} /* Fim função: PER  Destruir Perfil */
+
+/********** Fim do módulo de implementação: PER  Perfil  **********/
